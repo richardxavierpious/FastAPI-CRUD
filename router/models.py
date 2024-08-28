799d826ba66176e897f12ac8c3a4c346bdf59919
richardxavierpious <richardpious369@gmail.com>
2024-08-28 19:18:40 +0530

    added userProfile and Login classes

--- router/models.py
+++ router/models.py
@@ -4,3 +4,12 @@ class StudentData(BaseModel):
     name: str
     age: int
     grade: str
+
+class UserProfile(BaseModel):
+    userId: int
+    userName: str
+    userBio: str
+
+class Login(BaseModel):
+    userId: int
+    password: int
